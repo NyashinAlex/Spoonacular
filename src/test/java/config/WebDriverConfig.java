e001f61964dd682187592a2621d71bc06d6d1257
nyash <alex-nyashin1996@yandex.ru>
2022-12-12 21:20:56 +0300

    start new project

---
+++ src/test/java/config/WebDriverConfig.java
@@ -0,0 +1,30 @@
+package config;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources({
+        "classpath:${env}.properties"
+})
+
+public interface WebDriverConfig extends Config {
+
+    @Key("baseUrl")
+    @DefaultValue("https://spoonacular.com/")
+    String getBaseUrl();
+
+    @Key("browser")
+    @DefaultValue("chrome")
+    String getBrowser();
+
+    @Key("browserVersion")
+    @DefaultValue("100.0")
+    String getBrowserVersion();
+
+    @Key("browserSize")
+    @DefaultValue("1920x1080")
+    String getBrowserSize();
+
+    @Key("remoteWebDriver")
+    @DefaultValue("https://user1:1234@selenoid.autotests.cloud/wd/hub")
+    String getRemoteWebDriver();
+}
